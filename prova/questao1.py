01db812143d3963d28ace311f83883614f976647
Joao <joao.samuellc@gmail.com>
2022-10-13 14:22:28 -0300

    add *

---
+++ prova/questao1.py
@@ -0,0 +1,23 @@
+while True:
+
+    oper1 = float(input())
+    op = input()
+    oper2 = float(input())
+
+    if op == '+':
+        print(oper1 + oper2)
+
+    elif op == '-':
+        print(oper1 - oper2)
+
+    elif op == '/':
+        print(oper1 // oper2)
+
+    elif op == '*':
+        print(oper1 * oper2)
+
+
+
+
+
+
---
+++ prova/questao2.py
@@ -0,0 +1,8 @@
+def resolve_equacao():
+    c1 = float(input())
+    c2 = float(input())
+    x = c1 / -c2
+    return x
+
+
+resolve_equacao()
